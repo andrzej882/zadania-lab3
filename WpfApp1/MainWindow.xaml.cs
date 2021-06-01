e788cb632e34ab87e33c3deb7bfbb2b196a05a97
Andrzej <83847149+andrzej882@users.noreply.github.com>
2021-06-01 15:06:15 +0200

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtSuma_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+               
+                float x = float.Parse(TextBox1.Text);
+                float y = float.Parse(TextBox2.Text);
+
+                LabelWynik.Content = "Suma" + x + " + " + y + " = " + (x + y).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj dobre dane");
+            }
+        }
+
+        private void BtIloczyn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                float x = float.Parse(TextBox1.Text);
+                float y = float.Parse(TextBox2.Text);
+
+                LabelWynik.Content = "Iloczyn " + x + " * " + y + " = " + (x * y).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj dobre dane");
+            }
+        }
+
+        private void BtRoznica_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                float x = float.Parse(TextBox1.Text);
+                float y = float.Parse(TextBox2.Text);
+
+                LabelWynik.Content = "Roznica " + x + " - " + y + " = " + (x-y).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("podaj dobre dane");
+            }
+        }
+
+        private void BtIloraz_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            {
+               
+                float x = float.Parse(TextBox1.Text);
+                float y = float.Parse(TextBox2.Text);
+                if (y > 0)
+                {
+                    LabelWynik.Content = "Iloraz " + x + " / " + y + " = " + (x / y).ToString();
+                }
+                else
+                {
+                    
+                    MessageBox.Show("Nie mozesz dzielic prezz 0");
+                    
+                }
+            }
+            
+        }
+         
+            }
+        }
+    
+
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkbmw_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void butpokaz_Click(object sender, RoutedEventArgs e)
+        
+        {
+            if (checkbmw.IsChecked == true && checkrodz.IsChecked == true && radiodolna.IsChecked == true) 
+
+            {
+                textbox1.Text = "BMW X3";
+            }
+            else if (checkbmw.IsChecked == true && checkrodz.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "BMW X6";
+            }
+            else if (checkbmw.IsChecked == true && checksport.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "BMW M4";
+            }
+            else if (checkbmw.IsChecked == true && checksport.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "BMW E90";
+            }
+            else if (checkbmw.IsChecked == true && checkbus.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Nie posiadamy tego typu";
+            }
+            else if (checkbmw.IsChecked == true && checkbus.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Nie posiadamy tego typu";
+            }
+            else if (checkbmw.IsChecked == true && checkbus.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Nie posiadamy tego typu";
+            }
+            else if (checkford.IsChecked == true && checkbus.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Ford Transit";
+            }
+            else if (checkford.IsChecked == true && checkbus.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Ford Tourneo";
+            }
+            else if (checkford.IsChecked == true && checksport.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Ford Focus RS";
+            }
+            else if (checkford.IsChecked == true && checksport.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Ford Focus ST";
+            }
+            else if (checkford.IsChecked == true && checkrodz.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Ford S Max";
+            }
+            else if (checkford.IsChecked == true && checkrodz.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Ford Kuga";
+            }
+            else if (checkaudi.IsChecked == true && checkrodz.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Audi RS Q7";
+            }
+            else if (checkaudi.IsChecked == true && checkrodz.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Audi Q5";
+            }
+            else if (checkaudi.IsChecked == true && checksport.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Audi A5";
+            }
+            else if (checkaudi.IsChecked == true && checksport.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Audi RS7";
+            }
+            else if (checkaudi.IsChecked == true && checksport.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Audi RS7";
+            }
+            else if (checkaudi.IsChecked == true && checkbus.IsChecked == true && radiogorna.IsChecked == true)
+
+            {
+                textbox1.Text = "Nie posiadamy takowego modelu";
+            }
+            else if (checkaudi.IsChecked == true && checkbus.IsChecked == true && radiodolna.IsChecked == true)
+
+            {
+                textbox1.Text = "Nie posiadamy takowego modelu";
+            }
+            else
+            {
+                textbox1.Text = "Zaznacz odpowiednie parametry";
+            }
+
+        }
+    }
+}
